7c3e854ee6ef2402fe648f40bdc93b06979fcb60
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-08 19:33:09 -0500

    Welcome to the jungle

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var async = require('async');
+
 module.exports = {
-    
+    openDatabase: function () {
+        return;
+    },
+    fetchModels: function (_instance, models) {
+        console.log(models);
+    }
 };
