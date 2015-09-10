227d15e95ac9a8b196c2127edc0427f6074ecd0f
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-09 22:59:08 -0500

    Build models wrapper

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+/* jshint node: true, laxcomma: true */
 'use strict';
 
 var async = require('async');
 
+function wrapModel(_instance, model) {
+    var Model = require(model);
+
+    return function () {
+        return new Model(_instance.getComponents());
+    };
+}
+
 module.exports = {
-    openDatabase: function () {
-        return;
+    openDatabase: function (config) {
+        return q.resolve();
     },
     fetchModels: function (_instance, models) {
-        console.log(models);
+        var fetch = q.nbind(async.forEachOfSeries, async);
+
+        return fetch(models, function (path, model, next) {
+            var _model = wrapModel(_instance, path);
+
+            _instance.insertComponent(
+                model, _model, _instance.getComponents().models
+            );
+            next();
+        });
     }
 };
